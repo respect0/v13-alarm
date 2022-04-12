94fc6c3003b18e09deafe23a3bc23cee20f3f657
frkn :) <79569914+respect0@users.noreply.github.com>
2022-04-12 20:47:58 +0300

    fix

--- src/events/ready.js
+++ src/events/ready.js
@@ -15,65 +15,60 @@ async function alarmCheck(guild) {
   setInterval(async () => {
     let alarmData = await alarmDatabase.find();
     alarmData.map(async (data) => {
-      let dateNow = Date.now();
       let member = guild.members.cache.get(data.memberID);
       if (!member) return;
-      let dayName = moment(dateNow).format('dddd');
-      let hourAndMin = moment(dateNow).format('HH:mm');
-
-      const row = new MessageActionRow()
-        .addComponents(
-          new MessageButton()
-            .setLabel("Alarmı ertele")
-            .setStyle("PRIMARY")
-            .setCustomId("btnErtele"),
-          new MessageButton()
-            .setLabel("Alarmı kapat")
-            .setStyle("DANGER")
-            .setCustomId("btnKapat")
-        );
       if (!client.alarm.has(member.id)) {
-        if (data.alarm.gunler.pazartesi && data.alarm.saat == hourAndMin && dayName == "Pazartesi") {
-          console.log("calisti")
-          if (client.findChannel("alarm-log")) client.findChannel("alarm-log").send({ components: [row], content: `${member}, Pazartesi günü için \`${data.alarm.saat}\` saatine kurduğun alarm çalıyor.` }).then(async (msg) => {
-            alarmResult(data, member, msg);
-          });
-          if (!data.alarm.repeat) await alarmDatabase.updateOne({ memberID: data.memberID, alarmID: data.alarmID }, { $set: { alarm: { saat: data.alarm.saat, repeat: false, gunler: { pazartesi: false } } } });
-        } else if (data.alarm.gunler.sali && data.alarm.saat == hourAndMin && dayName == "Salı") {
-          if (client.findChannel("alarm-log")) client.findChannel("alarm-log").send({ components: [row], content: `${member}, Salı günü için \`${data.alarm.saat}\` saatine kurduğun alarm çalıyor.` }).then(async (msg) => {
-            alarmResult(data, member, msg);
-          });
-          if (!data.alarm.repeat) await alarmDatabase.updateOne({ memberID: data.memberID, alarmID: data.alarmID }, { $set: { alarm: { saat: data.alarm.saat, repeat: false, gunler: { sali: false } } } });
-        } else if (data.alarm.gunler.carsamba && data.alarm.saat == hourAndMin && dayName == "Çarşamba") {
-          if (client.findChannel("alarm-log")) client.findChannel("alarm-log").send({ components: [row], content: `${member}, Çarşamba günü için \`${data.alarm.saat}\` saatine kurduğun alarm çalıyor.` }).then(async (msg) => {
-            alarmResult(data, member, msg);
-          });
-          if (!data.alarm.repeat) await alarmDatabase.updateOne({ memberID: data.memberID, alarmID: data.alarmID }, { $set: { alarm: { saat: data.alarm.saat, repeat: false, gunler: { carsamba: false } } } })
-        } else if (data.alarm.gunler.persembe && data.alarm.saat == hourAndMin && dayName == "Perşembe") {
-          if (client.findChannel("alarm-log")) client.findChannel("alarm-log").send({ components: [row], content: `${member}, Perşembe günü için \`${data.alarm.saat}\` saatine kurduğun alarm çalıyor.` }).then(async (msg) => {
-            await alarmResult(data, member, msg)
-          });
-          if (!data.alarm.repeat) await alarmDatabase.updateOne({ memberID: data.memberID, alarmID: data.alarmID }, { $set: { alarm: { saat: data.alarm.saat, repeat: false, gunler: { persembe: false } } } });
-        } else if (data.alarm.gunler.cuma && data.alarm.saat == hourAndMin && dayName == "Cuma") {
-          if (client.findChannel("alarm-log")) client.findChannel("alarm-log").send({ components: [row], content: `${member}, Cuma günü için \`${data.alarm.saat}\` saatine kurduğun alarm çalıyor.` }).then(async (msg) => {
-            alarmResult(data, member, msg);
-          });
-          if (!data.alarm.repeat) await alarmDatabase.updateOne({ memberID: data.memberID, alarmID: data.alarmID }, { $set: { alarm: { saat: data.alarm.saat, repeat: false, gunler: { cuma: false } } } })
-        } else if (data.alarm.gunler.cumartesi && data.alarm.saat == hourAndMin && dayName == "Cumartesi") {
-          if (client.findChannel("alarm-log")) client.findChannel("alarm-log").send({ components: [row], content: `${member}, Cumartesi günü için \`${data.alarm.saat}\` saatine kurduğun alarm çalıyor.` }).then(async (msg) => {
-            alarmResult(data, member, msg);
-          });
-          if (!data.alarm.repeat) await alarmDatabase.updateOne({ memberID: data.memberID, alarmID: data.alarmID }, { $set: { alarm: { saat: data.alarm.saat, repeat: false, gunler: { cumartesi: false } } } });
-        } else if (data.alarm.gunler.pazar && data.alarm.saat == hourAndMin && dayName == "Pazar") {
-          if (client.findChannel("alarm-log")) client.findChannel("alarm-log").send({ components: [row], content: `${member}, Pazar günü için \`${data.alarm.saat}\` saatine kurduğun alarm çalıyor.` }).then(async (msg) => {
-            alarmResult(data, member, msg);
-          });
-          if (!data.alarm.repeat) await alarmDatabase.updateOne({ memberID: data.memberID, alarmID: data.alarmID }, { $set: { alarm: { saat: data.alarm.saat, repeat: false, gunler: { pazar: false } } } });
-        }
-      } else return;
+        dayCheck(member, data.alarm.gunler.pazartesi, data, "Pazartesi");
+        dayCheck(member, data.alarm.gunler.sali, data, "Salı");
+        dayCheck(member, data.alarm.gunler.carsamba, data, "Çarşamba");
+        dayCheck(member, data.alarm.gunler.persembe, data, "Perşembe");
+        dayCheck(member, data.alarm.gunler.cuma, data, "Cuma");
+      }
+
     })
   }, 1000)
 }
+
+//@Fahrettin Enes#9583 - 875060734969655307
+async function dayCheck(member, gun, data, gunString) {
+  let dayName = moment().format('dddd');
+  let hourAndMin = moment().format('HH:mm');
+
+  if (gun && data.alarm.saat == hourAndMin && dayName == gunString) {
+    const row = new MessageActionRow()
+      .addComponents(
+        new MessageButton()
+          .setLabel("Alarmı ertele")
+          .setStyle("PRIMARY")
+          .setCustomId("btnErtele"),
+        new MessageButton()
+          .setLabel("Alarmı kapat")
+          .setStyle("DANGER")
+          .setCustomId("btnKapat")
+      );
+    if (client.findChannel("alarm-log")) client.findChannel("alarm-log").send({ components: [row], content: `${member}, ${gunString} günü için \`${data.alarm.saat}\` saatine kurduğun alarm çalıyor.` }).then(async (msg) => {
+      alarmResult(data, member, msg);
+    });
+    if (!data.alarm.repeat) {
+      await alarmDatabase.updateOne({
+        memberID: member.id,
+        alarmID: data.alarmID
+      },
+        {
+          $set: {
+            alarm: {
+              saat: data.alarm.saat,
+              repeat: false,
+              gunler: {
+                [gunString.toLowerCase()]: false
+              }
+            }
+          }
+        });
+    }
+  }
+}
+
 
 async function alarmResult(data, member, msg) {
   client.alarm.set(member.id);
