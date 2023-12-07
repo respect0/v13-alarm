72deeb9837e5b8b3df6e7fada2298a340c0063c1
frkn :) <79569914+respect0@users.noreply.github.com>
2023-12-07 15:33:41 +0300

    Delete src directory

--- src/models/guild.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-
-module.exports = mongoose.model("guild", mongoose.Schema({
-    guildID: {type: String, default: null},
-}));
--- src/util/eventLoader.js
+++
@@ -1,6 +0,0 @@
-const resEvent = event => require(`../events/${event}`);
-
-module.exports = client => {
-    client.on('ready', () => resEvent('ready')(client));
-    client.on('messageCreate', resEvent('message'));
-};
